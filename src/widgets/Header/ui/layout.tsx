f253db226282fee8be983475968b5274e1765534
Nikita Egunov <negunov1003@gmail.com>
2025-06-22 20:38:40 +0300

    Add "sticky" header

--- src/widgets/Header/ui/layout.tsx
+++ src/widgets/Header/ui/layout.tsx
@@ -3,7 +3,7 @@ import { RightText } from "./RightText";
 
 function Layout() {
   return (
-    <header className="flex justify-between py-[10px] bg-lime-700 px-[20px]">
+    <header className="flex justify-between py-[10px] bg-lime-700 px-[20px] sticky top-0 left-0">
       <Logo />
       <RightText />
     </header>
